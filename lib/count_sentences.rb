dcefeafb892c06025bf5df59fea6bb5e19f2718b
Chenyun Zhang <morningchenyun@gmail.com>
2020-08-04 21:30:55 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    self.split(" ").each{|val|
+    if val.sentence? || val.exclamation? || val.question?
+      count+=1
+    end
+  }
+  count
   end
 end
